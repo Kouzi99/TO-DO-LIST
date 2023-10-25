15155aeee774c6ee27ecee6a648af54585592736
Kouzi99 <125484339+Kouzi99@users.noreply.github.com>
2023-10-25 13:38:43 +0200

    removed remove

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # TO-DO-LIST
 
 Basic todo list created just by using of html,css and pure javascript code.
+
 <img src="./readme/Screenshot 2023-10-25 122308.png" alt="Alt text" title="Optional title">
--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const taskInput = document.getElementById("taskInput");
 const addTaskButton = document.getElementById("addTask");
 const taskList = document.getElementById("taskList");
 
+// Load tasks from local storage when the page loads
+document.addEventListener("DOMContentLoaded", loadTasks);
+
 // Add task when clicking the "Add Task" button
 addTaskButton.addEventListener("click", addTask);
 
@@ -24,10 +27,11 @@ function addTask() {
 
         // Add a button to remove the task
         const removeButton = document.createElement("button");
-        removeButton.classList.add("button-13")
+        removeButton.classList.add("button-13");
         removeButton.innerText = "Remove";
         removeButton.addEventListener("click", function () {
             taskList.removeChild(taskItem);
+            updateLocalStorage();
         });
 
         taskItem.appendChild(removeButton);
@@ -35,6 +39,43 @@ function addTask() {
 
         // Clear the input field
         taskInput.value = "";
+
+        // Update local storage (excluding the "Remove" button text)
+        updateLocalStorage();
     }
 }
+
+// Load tasks from local storage
+function loadTasks() {
+    const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    tasks.forEach(function (taskText) {
+        // Create a new task item
+        const taskItem = document.createElement("li");
+        taskItem.innerText = taskText;
+
+        // Add a button to remove the task
+        const removeButton = document.createElement("button");
+        removeButton.classList.add("button-13");
+        removeButton.innerText = "Remove";
+        removeButton.addEventListener("click", function () {
+            taskList.removeChild(taskItem);
+            updateLocalStorage();
+        });
+
+        taskItem.appendChild(removeButton);
+        taskList.appendChild(taskItem);
+    });
+}
+
+// Update local storage with the current tasks (excluding the "Remove" button text)
+function updateLocalStorage() {
+    const tasks = [];
+    const taskItems = document.querySelectorAll("li");
+    taskItems.forEach(function (taskItem) {
+        tasks.push(taskItem.innerText.replace("Remove", "").trim());
+    });
+
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
 
